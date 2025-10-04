c62b70de93e078a863ee9686ab64e055713663a7
Majoson <42291113+majoson-chen@users.noreply.github.com>
2025-10-04 10:04:12 +0800

    feat: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,14 +4,16 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
     formatters: true,
     vue: true,
+    typescript: true,
     stylistic: {
         indent: 4,
         quotes: 'single',
         semi: false,
-        overrides: {
-            'style/curly-newline': ['warn', {
-                multiline: true,
-            }],
-        },
+    },
+    markdown: true,
+    rules: {
+        'style/curly-newline': ['warn', {
+            multiline: true,
+        }],
     },
 })
